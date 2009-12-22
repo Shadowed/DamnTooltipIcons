08894e5fdb11f7e39249cf03444571619e082ce2
Shadowed <shadowed.wow@gmail.com>
2009-12-21 23:46:05 -0800

    Added support for achievement and spell links

--- DamnTooltipIcons.lua
+++ DamnTooltipIcons.lua
@@ -1,21 +1,28 @@
-local function restorePositions()
-	ItemRefTooltipTexture10:Hide()
-
-	ItemRefTooltipTextLeft1:ClearAllPoints()
-	ItemRefTooltipTextLeft1:SetPoint("TOPLEFT", ItemRefTooltip, "TOPLEFT", 8, -10)
-
-	ItemRefTooltipTextLeft2:ClearAllPoints()
-	ItemRefTooltipTextLeft2:SetPoint("TOPLEFT", ItemRefTooltipTextLeft1, "BOTTOMLEFT", 0, -2)
-end
+--[[
+	Shadow, Mal'Ganis (US)
+]]
 
 hooksecurefunc("SetItemRef", function(link, text, button)
-	local icon = select(10, GetItemInfo(link))
-	if( not icon or not string.match(link, "item:") ) then
-		restorePositions()
+	local icon
+	local type, id = string.match(link, "^([a-z]+):(%d+)")
+	if( type == "item" ) then
+		icon = select(10, GetItemInfo(link))
+	elseif( type == "spell" or type == "enchant" ) then
+		icon = select(3, GetSpellInfo(id))
+	elseif( type == "achievement" ) then
+		icon = select(10, GetAchievementInfo(id))
+	end	
+	
+	if( not icon ) then
+		ItemRefTooltipTexture10:Hide()
+
+		ItemRefTooltipTextLeft1:ClearAllPoints()
+		ItemRefTooltipTextLeft1:SetPoint("TOPLEFT", ItemRefTooltip, "TOPLEFT", 8, -10)
+
+		ItemRefTooltipTextLeft2:ClearAllPoints()
+		ItemRefTooltipTextLeft2:SetPoint("TOPLEFT", ItemRefTooltipTextLeft1, "BOTTOMLEFT", 0, -2)
 		return
 	end
-	
-	
 
 	ItemRefTooltipTexture10:ClearAllPoints()
 	ItemRefTooltipTexture10:SetPoint("TOPLEFT", ItemRefTooltip, "TOPLEFT", 8, -7)
